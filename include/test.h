57c99fa40ab5ef7c12b24dee4fbff84d97c7c4e8
niupanyu <niupanyu@126.com>
2018-04-22 18:08:57 +0800

    增加单元测试组件 catch2

---
+++ include/test.h
@@ -0,0 +1,7 @@
+#ifndef __TEST__H_
+#define __TEST__H_
+
+#define MYTEST 0
+
+
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+#include "test.h"
+int main(){
+    std::string tmp  = "ee";
+    std::string str = "abc"  "123";
+    std::cout << str << std::endl;
+    std::cout << MYTEST << std::endl;
+}
