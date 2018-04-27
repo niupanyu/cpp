269a6a207c78898fe85a0056466d974355c8dd1c
jackyniu <jackyniu@tencent.com>
2018-04-27 16:54:04 +0800

    增加c++11预编译支持

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include<vector>
 
 #include "test.h"
+
+void for_range_demo(){
+    std::vector<int> ivec{0,1,2,3,4,5};
+    for(auto n : ivec){
+        std::cout << n << " " ;        
+    }
+}
+
+
 int main(){
     std::string tmp  = "ee";
     std::string str = "abc"  "123";
     std::cout << str << std::endl;
     std::cout << MYTEST << std::endl;
+    
+    for_range_demo();
 }
