1fcb26adf4da585cefc45bc4628ebc98c1e2068d
jackyniu <jackyniu@tencent.com>
2019-04-25 17:32:41 +0800

    functor

---
+++ test/functor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Functor
+{
+    int x, y;
+    public:
+        Functor()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        Functor(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+        int operator()(){
+            return x+y;
+        }
+
+};
+
+int main()
+{
+    Functor f(1,3);
+
+    cout << f() << endl;
+
+
+
+    return 0;
+};
